0c4020eaa607c864fd4287e2afe10481a4f7a744
id-def01t <62650141+id-def01t@users.noreply.github.com>
2020-09-28 00:09:07 +0300

    Add files via upload

--- 1 lab 13 var/lab1.cpp
+++ 1 lab 13 var/lab1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "header.h"
+#include "13.h"
 
 
 Line* input(int& rm, int& bm) 
@@ -12,7 +12,7 @@ Line* input(int& rm, int& bm)
 	{
 		std::cout << pr << std::endl;
 		pr = "Repeat pls";
-		std::cout << "Enter number of rows: --> ";
+		std::cout << "Ведите количество строк --> ";
 		if (getNum(row) < 0)	
 		{
 			return nullptr;
@@ -23,7 +23,7 @@ Line* input(int& rm, int& bm)
 	do {
 		std::cout << pr1 << std::endl;
 		pr1 = "Repeat pls";
-		std::cout << "Enter number of bars: --> ";
+		std::cout << "Введите количество столбцов --> ";
 		if (getNum(bar) < 0)
 		{
 			return nullptr;
@@ -40,35 +40,72 @@ Line* input(int& rm, int& bm)
 		std::cout << "------- too many rows in matrix: " << ba.what() << std::endl;
 		return nullptr;
 	}
-	for (int i = 0; i < row; ++i) 
+	for (int i = 0; i < row; ++i)
 	{
 		lines[i].root = nullptr;
 		lines[i].num = 0;	// Число ненулевых ячеек
-		std::cout << "Enter items for matrix line #" << (i + 1) << ":" << std::endl;
-		for (int j = 0; j < bar; ++j) 
+	}
+	int k = 1, p = 1;
+	while (k > 0)
+	{
+		std::cout << "В какую строку вы хотите добавить ненулевой элемент?(введите отрицательный если хотите выйти) --> ";
+		if (getNum(k) < 0)
 		{
-			int k;
-			if (getNum(k) < 0) 
-			{
-				erase(lines, i + 1);
-				return nullptr;
-			}
-			else if (k > 0) 
-			{
-				lines->num += 1;
-				Item* tmp = new Item[1];	// Создаем список хранящий в себе ненулевые элементы
-				tmp->j = j;		// Запоминаем индекс такового
-				tmp->n = k;		// И его значение
-				tmp->next = nullptr;
-				Item* last = nullptr;
-				if (lines[i].root == nullptr) 
+			erase(lines, row);
+			return nullptr;
+		}
+		else if (k == 0)
+		{
+			std::cout << "Номер строки начинается с 1..." << std::endl;
+		}
+		else if (k > row)
+		{
+			std::cout << "Ваш номер должен быть меньше либо равен " << row << std::endl;
+		}
+		else if (k > 0)
+		{
+			while (true) {
+				std::cout << "Введите номер столбца для добваления(введите отрицательный если хотите выйти) --> ";
+				if (getNum(p) < 0)
 				{
-					lines[i].root = tmp;
+					erase(lines, row);
+					return nullptr;
 				}
-				else
+				if (p < 0)
 				{
-					last = getLast(lines[i].root);
-					last->next = tmp;
+					break;
+				}
+				else if (p == 0)
+				{
+					std::cout << "Номер должен начинаться с 1..." << std::endl;
+				}
+				else if (p > bar)
+				{
+					std::cout << "Ваш номер должен быть меньше либо равен " << bar << std::endl;
+				}
+				else if (p > 0)
+				{
+					std::cout << "Введите ненулевой элемент для " << k << " строки, " << p << " столбца" << std::endl;
+					int num;
+					if (getNum(num) < 0)
+					{
+						erase(lines, row);
+						return nullptr;
+					}
+					Item* tmp = new Item[1];	// Создаем список хранящий в себе ненулевые элементы
+					tmp->j = p - 1;		// Запоминаем индекс такового
+					tmp->n = num;		// И его значение
+					tmp->next = nullptr;
+					Item* last = nullptr;
+					if (lines[k - 1].root == nullptr)
+					{
+						lines[k - 1].root = tmp;
+					}
+					else
+					{
+						last = getLast(lines[k - 1].root);
+						last->next = tmp;
+					}
 				}
 			}
 		}
@@ -102,21 +139,14 @@ void output(const char* msgs, Line* lines, int row, int bar)
 		Item* tmp = lines[i].root;
 		for (int j = 0; j < bar; ++j)
 		{
-			if (tmp)
+			if (tmp && tmp->j == j)
 			{
-				if (tmp->j == j) 
+				std::cout << tmp->n << " ";
+				if (tmp->next)
 				{
-					std::cout << tmp->n << " ";
-					if (tmp->next)
-					{
-						tmp = tmp->next;
-					}
+					tmp = tmp->next;
 				}
-				else
-				{
-					std::cout << "0" << " ";
-				}
-			}
+		}
 			else
 			{
 				std::cout << "0" << " ";
