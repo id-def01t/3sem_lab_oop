a1fa9a7d9d9675bb1506de64c10089f0627e9ad1
id-def01t <62650141+id-def01t@users.noreply.github.com>
2020-09-28 00:10:04 +0300

    1 лаба 13 вариант

--- 1 lab 13 var/mian.cpp
+++ 1 lab 13 var/mian.cpp
@@ -1,19 +1,20 @@
-#include "header.h"
+#include "13.h"
 
 
 int main() 
 {
+	setlocale(LC_ALL, "Russian");
 	Line* arr = nullptr;
 	int row;				// Число строк
 	int bar;				// Число столбцов
 	arr = input(row, bar);	
 	if (!arr) 
 	{
-		std::cout << "incorrect data" << std::endl;
+		std::cout << "Неверные данные" << std::endl;
 		return 1;
 	}
-	output("Sourced matrix", arr, row, bar);	// Вывод исходной матрицы
-	solve("This is the vector: ", arr, row, bar);
+	output("Исходная матрица", arr, row, bar);	// Вывод исходной матрицы
+	solve("Результирующий вектор: ", arr, row, bar);
 	erase(arr, row);							// Освобождение памяти
 	return 0;
 }
